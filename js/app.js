b70b299b4f70da5f70e5d4d02a8232f08f8aa706
zty <tonalll@qq.com>
2015-11-04 16:52:47 +0800

    监听窗口大小变化

--- js/app.js
+++ js/app.js
@@ -1,13 +1,46 @@
 'use strict';
 var ngw = angular.module('ngw', []);
-ngw.controller('mycontroller', ['$scope', 
-    function ($scope,$routeParams) {
+ngw.controller('mycontroller', ['$scope',
+    function ($scope, $routeParams) {
         console.info($scope);
         $scope.$on('$viewContentLoaded', function () {
             console.info(1);
         });
         console.info(2);
-        $scope.move=function(){
+        $scope.move = function () {
             console.info('move');
         }
     }])
+
+
+//监听窗口变化
+ngw.directive('resize', function ($window) {
+    return function (scope, element) {
+        var w = angular.element($window);
+        scope.getWindowDimensions = function () {
+            return {
+                'h': w.height(),
+                'w': w.width()
+            };
+        };
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+            scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+
+            scope.style = function () {
+                return {
+                    'height': (newValue.h - 100) + 'px',
+                    'width': (newValue.w - 100) + 'px'
+                };
+            };
+
+        }, true);
+
+        w.bind('resize', function () {
+            console.info(w.width());
+            console.info(w.height());
+            scope.$apply();
+        });
+
+    }
+})
